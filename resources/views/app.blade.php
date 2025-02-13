2513bc158ad0561b4e11acabf158d55c63ea2d5c
Univers1994 <tononregis67@gmail.com>
2025-02-13 09:50:03 +0100

    Modification

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="no-underline text-3xl font-bold underline bg-sky-600 text-white p-4 text-center hover:bg-green-600 text-6xl h">
+      Hello world!
+    </h1>
+
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sapiente adipisci fuga. Deserunt, necessitatibus. Dolore, enim quibusdam possimus cum officia neque? Ratione, accusantium nostrum. Labore id quae error corporis quidem.</p>
+    </div>
+  </body>
+</html>
